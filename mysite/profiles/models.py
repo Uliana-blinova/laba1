9455ab789c7a45bb301c69aecc6c7cd02fd10122
Uliana Blinova <ulanablinova0@gmail.com>
2025-11-28 16:33:07 +0700

    add profile

---
+++ mysite/profiles/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(upload_to='avatars/', help_text="Аватар обязателен")
+
+    def __str__(self):
+        return f"Профиль {self.user.username}"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
